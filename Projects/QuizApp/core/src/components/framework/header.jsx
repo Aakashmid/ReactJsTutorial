5dc0d50efbd9cf14ec8826a19a580c936141a587
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-07-11 18:59:12 +0530

    install   material ui

---
+++ Projects/QuizApp/core/src/components/framework/header.jsx
@@ -0,0 +1,13 @@
+
+
+import AppBar from '@mui/material/AppBar';
+
+export default function header() {
+  return (
+    <>
+        <AppBar sx={{flexGrow:1}}>
+            Hello
+        </AppBar>
+    </>
+  )
+}
