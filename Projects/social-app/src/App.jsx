3b210cc076a32f6f3bceb8d4511446f2f1da44f4
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-07-16 16:37:22 +0530

    Create routing in socialApp

--- Projects/social-app/src/App.jsx
+++ Projects/social-app/src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
-import PersonIcon from '@mui/icons-material/Person';
+
 import Home from './pages/home/home';
 import Profile from './pages/profile/profile';
 import Login from './pages/login/login';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 
 function App() {
-  return <Profile/>
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' Component={Home}/>
+        <Route path='/profile' Component={Profile}/>
+        <Route path='/login' Component={Login}/>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
--- Projects/social-app/src/components/topbar/Topbar.jsx
+++ Projects/social-app/src/components/topbar/Topbar.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom'
 import './topbar.css'
-import { Search,Person,Chat,Notifications } from '@mui/icons-material'
+import { Search, Person, Chat, Notifications } from '@mui/icons-material'
 export default function Topbar() {
     return (
         <div className="topbarContainer">
             <div className="topbarLeft">
-                <span className="logo">SocialApp</span>
+                <Link style={{textDecoration:'none'}} to='/'><span className="logo" >SocialApp</span></Link>
             </div>
             <div className="topbarCenter">
                 <div className="searchbar">
@@ -15,25 +16,27 @@ export default function Topbar() {
             </div>
             <div className="topbarRight">
                 <div className="topbarlinks">
-                    <div className="topbarlink">Homepage</div>
+                    <Link style={{textDecoration:'none',color:'white'}} to='/'><div className="topbarlink">Homepage</div></Link>
                     <div className="topbarlink">Timeline</div>
                 </div>
                 <div className="topbarIcons">
 
-                <div className="topbarIconItem">
-                    <Person/>
-                    <span className="topbarIconBadge">1</span>
+                    <div className="topbarIconItem">
+                        <Person />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Chat />
+                        <span className="topbarIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Notifications />
+                        <span className="topbarIconBadge">3</span>
+                    </div>
                 </div>
-                <div className="topbarIconItem">
-                    <Chat/>
-                    <span className="topbarIconBadge">2</span>
-                </div>
-                <div className="topbarIconItem">
-                    <Notifications/>
-                    <span className="topbarIconBadge">3</span>
-                </div>
-                </div>
-                <img className='topbarImg' src="/assets/person/1.jpeg" alt="" />
+                <Link style={{ textDecoration: 'none' }} to='/profile'>
+                    <img className='topbarImg' src="/assets/person/1.jpeg" alt="" />
+                </Link>
             </div>
         </div>
     )
