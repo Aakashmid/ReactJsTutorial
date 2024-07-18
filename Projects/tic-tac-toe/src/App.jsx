a73ca09ef4ff4ac245752b4dad7c1aa683187a23
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-07-18 18:05:05 +0530

    modified App.jsx

--- Projects/tic-tac-toe/src/App.jsx
+++ Projects/tic-tac-toe/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const Square = ({ value, onSquareClick }) => {
   return (
-    <li className="square w-24 h-20 text-3xl text-center  py-4 border border-gray-300 list-none cursor-pointer" onClick={onSquareClick}>{value}</li>
+    <li className={`square w-24 h-20 text-3xl text-center  py-4 border border-gray-300 list-none cursor-pointer ${value === 'X' ? 'text-yellow-600' : 'text-black'} `} onClick={onSquareClick}>{value}</li>
   )
 }
 
@@ -23,15 +23,16 @@ function calculateWinner(squares) {
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
+
   }
   return null;
 }
 
 
 
-function Board({ xIsNext, squares, onPlay }) {
+function Board({ xIsNext, squares, onPlay, tie }) {
   function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
+    if (calculateWinner(squares) || squares[i]) {   // if winner is diclared or clicking a square  which already have a value then return to stop further execution
       return;
     }
     const nextSquares = squares.slice();
@@ -44,33 +45,33 @@ function Board({ xIsNext, squares, onPlay }) {
   }
 
 
+
   let winner = calculateWinner(squares);
   let status;
-  if (winner) {
+  if (winner || tie) {
     status = 'Game Over'   // status for tracking which player's move is 
   }
   else {
-    status =  (xIsNext ? 'X ' : 'O ') + "ki bajji ";   // status for tracking which player's move is 
+    status = (xIsNext ? 'X ' : 'O ') + "ki bajji ";   // status for tracking which player's move is 
   }
 
 
   return (
     <>
 
-      <div className="status text-center my-4 text-xl font-semibold p-2 bg-gradient-to-r from-yellow-800 to-amber-100 rounded-lg">{status}</div>
-      <div className="bg-white">
-
-        <div className="board-row flex">
+      <div className="status text-center my-4 text-xl font-semibold p-2 bg-gradient-to-r from-yellow-700 to-amber-200 rounded-lg"> {status}</div>
+      <div className="">
+        <div className="board-row flex bg-white  ">
           <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
           <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
           <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
         </div>
-        <div className="board-row flex">
+        <div className="board-row flex bg-white ">
           <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
           <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
           <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
         </div>
-        <div className="board-row flex">
+        <div className="board-row flex bg-white ">
           <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
           <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
           <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
@@ -98,6 +99,7 @@ export default function Game() {
 
   function jumpTo(nextMove) {  // for moving to previous board state
     setCurrentMove(nextMove);
+    setHistory([history[0]])
   }
 
 
@@ -116,17 +118,26 @@ export default function Game() {
     );
   });
 
+
+  let tie = null;
+  if (!winner && history.length == 10) {
+    tie = true
+  }
+  
   return (
     <div className="container ">
       <div className="game lg:mt-20 mt-32 flex flex-col lg:flex-row items-center lg:justify-around lg:w-[70vw] mx-auto ">
         <div className="game-board">
-          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} tie={tie} />
         </div>
-        <div className="result-box mt-4 text-center">{winner ? 
-        <>
-        <p className="text-white font-semibold text-3xl">Winner is  {winner}</p>
-        <button type="button" onClick={()=>jumpTo(0)} className="py-2 px-3 mt-3 text-yellow-900 bg-white text-xl font-medium rounded">Restart Game</button>
-        </> : ""}</div>
+        {winner  && (<div className="result-box mt-4 text-center">
+          <p className="text-white font-semibold text-3xl">{winner && 'Winner is '+winner}</p>
+          <button type="button" onClick={() => jumpTo(0)} className="py-2 px-3 mt-3 text-yellow-900  bg-white hover:bg-amber-200 text-xl font-medium rounded">Restart Game</button>
+        </div>)}
+        {tie  && (<div className="result-box mt-4 text-center">
+          <p className="text-white font-semibold text-3xl">{tie && 'Draw'}</p>
+          <button type="button" onClick={() => jumpTo(0)} className="py-2 px-3 mt-3 text-yellow-900  bg-white hover:bg-amber-200 text-xl font-medium rounded">Restart Game</button>
+        </div>)}
         <div className="game-info mt-10 hidden">
           <ol >{moves}</ol>
         </div>
