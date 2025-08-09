428358384ede8fd6e173389ef40548f42e2cfcda
Aakash Kumar Jha <aakash13102005@gmail.com>
2025-08-10 01:40:41 +0530

    modified

--- Projects/3D-Portfolio/portfolio-ui/src/components/Hero.jsx
+++ Projects/3D-Portfolio/portfolio-ui/src/components/Hero.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <div className=''>Hero</div>
+    <section className='w-full relative h-[100dvh]  mx-auto '>
+      <div className={`${styles.paddingX}  absolute inset-0 top-[7.5rem] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
+          <div className="w-1 h-40 sm:h-80 violet-gradient"></div>
+        </div>
+
+        <div className="">
+          <h1 className={`${styles.heroHeadText}`}>Hi, I'm  <span className="text-[#915eff]"> Aakash</span></h1>
+          <p className={`${styles.heroSubText}`}>I develop 3D visuals , user interfaces and web applications</p>
+        </div>
+      </div>
+
+      <ComputersCanvas/>
+    </section>
   )
 }
 
-export default Hero
+export default Hero;
--- Projects/3D-Portfolio/portfolio-ui/src/components/canvas/Computers.jsx
+++ Projects/3D-Portfolio/portfolio-ui/src/components/canvas/Computers.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { Suspense,useEffect , useState } from "react";
+import {Canvas} from '@react-three/fiber';
+import {OrbitControls,Preload ,useGLTF} from '@react-three/drei';
 
+
+// go to react three fiber docs for detail information
 const Computers = () => {
+
+  const computer = useGLTF('./desktop_pc/scene.gltf');
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15}  groundColor={'black'}/>
+    </mesh>
   )
 }
 
